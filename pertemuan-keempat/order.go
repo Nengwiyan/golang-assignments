3b80f2cb242a94aa478f0111c30890b0a609b89e
Neneng Wiyanti <114837384+Nengwiyan@users.noreply.github.com>
2023-08-28 08:00:42 +0700

    Update order.go

--- pertemuan-keempat/order.go
+++ pertemuan-keempat/order.go
@@ -16,19 +16,18 @@ func main() {
 	dataDua = []string{"bisa1", "bisa2", "bisa3"}
 
 	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go cetak(dataSatu, dataDua, i+1)
+		wg.Add(2)
+		mtx.Lock()
+		go cetak(dataSatu, i+1, &wg, &mtx)
+		mtx.Lock()
+		go cetak(dataDua, i+1, &wg, &mtx) 
 	}
 
 	wg.Wait()
 }
 
-func cetak(d interface{}, e interface{}, num int) {
-	mtx.Lock()
-	defer mtx.Unlock()
-
+func cetak(d interface{}, num int, wg &sync.WaitGroup, mtx *sync.Mutex) {
 	fmt.Println(d, num)
-	fmt.Println(e, num)
-
+	mtx.Unlock()
 	wg.Done()
 }
